4a283e8779126c74988230558da926800114b36a
braveocheretovych <braveocheretovych@gmail.com>
2026-04-03 10:16:04 +0300

    fix(store): return YAML error instead of JSON error when parsing twurlrc
    Signed-off-by: braveocheretovych <braveocheretovych@gmail.com>

--- store/tokens.go
+++ store/tokens.go
@@ -337,7 +337,7 @@ func (s *TokenStore) importFromTwurlrc(filePath string) error {
 	}
 
 	if err := yaml.Unmarshal(data, &twurlConfig); err != nil {
-		return errors.NewJSONError(err)
+		return errors.NewYAMLError(err)
 	}
 
 	app := s.activeAppOrCreate()
